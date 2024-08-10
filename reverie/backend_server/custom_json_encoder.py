8849b67b878f94ec8c23d85fad10ce5e7d15de26
Gustav Radloff <danielrad082@gmail.com>
2024-08-10 17:26:45 +0200

    Updated encoder to simplify class attribute names

--- reverie/backend_server/custom_json_encoder.py
+++ reverie/backend_server/custom_json_encoder.py
@@ -1,9 +1,18 @@
 import json
 import datetime
+import re
 from typing import Any
 
 class CustomJSONEncoder(json.JSONEncoder):
+  # vars is used to pass objects to the encoder
+  # the output of vars when it encouters private attributes is 
+  # a bit "mangled", this regex and case in the encoder "demangles"
+  # the class attribute, improving readability.
+  _class_private_attribute_regex = re.compile(r'^_[A-Z]([A-Z]|[a-z])+__([a-z]|_)*[a-z]$')
+
   def default(self, o: Any) -> Any:
     if isinstance(o, datetime.datetime):
       return o.strftime("%B %d, %Y, %H:%M:%S")
+    if isinstance(o,str) and self._class_private_attribute_regex.match(o):
+      return o.split("__")[1]
     return super().default(o)
